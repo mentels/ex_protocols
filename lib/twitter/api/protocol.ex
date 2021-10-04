c48819ca6e38d2d4b3f4e78fa93cc98beee49852
Szymon Mentel <mentel.szymon@gmail.com>
2021-10-04 10:42:45 +0200

    Add defimpl to defprocotol file

--- lib/twitter/api/protocol.ex
+++ lib/twitter/api/protocol.ex
@@ -5,3 +5,10 @@ defprotocol Protos.Twitter.API.Protocol do
   @spec register(struct(), binary) :: :ok | :already_taken
   def get_by_username(impl, username)
 end
+
+defimpl Protos.Twitter.API.Protocol, for: Protos.Twitter.Stub.Stateful do
+  alias Protos.Twitter.Stub.Stateful, as: Impl
+
+  defdelegate register(impl, username), to: Impl
+  defdelegate get_by_username(impl, username), to: Impl
+end
--- lib/twitter/stub/statefull.ex
+++ lib/twitter/stub/statefull.ex
@@ -1,7 +1,6 @@
 defmodule Protos.Twitter.Stub.Stateful do
   defstruct [:pid]
 
-  alias Protos.Twitter.API.Protocol, as: TwitterAPI
   alias Protos.Twitter.User
 
   def new(opts \\ []) do
@@ -13,22 +12,20 @@ defmodule Protos.Twitter.Stub.Stateful do
     Agent.start_link(&Map.new/0)
   end
 
-  defimpl TwitterAPI do
-    def register(%{pid: pid}, username) do
-      Agent.get_and_update(pid, fn
-        %{^username => _user} = users ->
-          {:already_taken, users}
+  def register(%{pid: pid}, username) do
+    Agent.get_and_update(pid, fn
+      %{^username => _user} = users ->
+        {:already_taken, users}
 
-        users ->
-          {:ok, Map.put_new(users, username, %User{username: username})}
-      end)
-    end
+      users ->
+        {:ok, Map.put_new(users, username, %User{username: username})}
+    end)
+  end
 
-    def get_by_username(%{pid: pid}, username) do
-      Agent.get(pid, fn
-        %{^username => user} -> user
-        _ -> nil
-      end)
-    end
+  def get_by_username(%{pid: pid}, username) do
+    Agent.get(pid, fn
+      %{^username => user} -> user
+      _ -> nil
+    end)
   end
 end
